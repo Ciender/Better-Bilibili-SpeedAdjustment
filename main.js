3fefddebb3db94283b590a75860e035a70c7659e
zacll <1260997304@qq.com>
2025-03-25 00:40:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         HTML5播放器自定义倍速
+// @name         B站播放器视频倍速调节
 // @namespace    http://tampermonkey.net/
 // @version      1.4
-// @description  滚轮调节+异步弹窗+C键切换+绿色提示+[修复卡顿]
+// @description  滚轮调节+异步弹窗+C键切换+绿色提示+[修复卡顿]+兼容原生
 // @author       deepseek
 // @match        *://*/*
 // @grant        none
